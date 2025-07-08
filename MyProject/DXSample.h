4272bf2f910d3c4581982b39c1e62fc9b8b001f0
k03302 <unisonman660@gmail.com>
2025-07-09 00:58:02 +0900

    Change DXSample signature types

--- MyProject/DXSample.h
+++ MyProject/DXSample.h
@@ -1,19 +1,20 @@
 #pragma once
+#include <windows.h>
 
 class DXSample
 {
 public:
-	DXSample(int width, int height);
+	DXSample(UINT width, UINT height);
 	virtual ~DXSample() = default;
-	virtual void Initialize(class HWND hwnd, int width, int height) = 0;
-	virtual void Update() = 0;
-	virtual void Render() = 0;
-	virtual void Destroy() = 0;
-	virtual void OnResize(int width, int height) = 0;
+	virtual BOOL Initialize(HWND hwnd, UINT width, UINT height) = 0;
+	virtual bool Update() = 0;
+	virtual bool Render() = 0;
+	virtual bool Destroy() = 0;
+	virtual bool OnResize(UINT width, UINT height) = 0;
 	
-	int GetHeight();
-	int GetWidth();
+	UINT GetHeight();
+	UINT GetWidth();
 
 protected:
-	int m_height, m_width;
+	UINT m_height, m_width;
 };
